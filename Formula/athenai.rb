e72ecf8fcd3d42b1fccab812ac2c9ab019b282e4
goreleaserbot <goreleaser@carlosbecker.com>
2017-08-07 07:58:48 +0900

    athenai version 0.1.2

--- Formula/athenai.rb
+++ Formula/athenai.rb
@@ -2,9 +2,9 @@ class Athenai < Formula
   desc "Athenai is a simple and easy-to-use command line tool that runs SQL query statements on Amazon Athena.
 "
   homepage "https://github.com/skatsuta/athenai"
-  url "https://github.com/skatsuta/athenai/releases/download/0.1.1/athenai_macOS_64-bit.tar.gz"
-  version "0.1.1"
-  sha256 "e05d63c6afc0a8d74f162cf153f132435a9c74b77a8c9c02ab562f4483d0e45f"
+  url "https://github.com/skatsuta/athenai/releases/download/0.1.2/athenai_macOS_64-bit.tar.gz"
+  version "0.1.2"
+  sha256 "fed86deb97f146009a22d42d9f2107bda2578679220d797223f7d36e65e81874"
 
   def install
     bin.install "athenai"
